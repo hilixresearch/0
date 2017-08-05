5df2be2964567cb4439529280d3f73d44212ab60
Luqman Malik <luqmanmalik@hilixr.com>
2017-08-04 17:16:19 -0700

    update_repo
    add dashboard, database UI, profile_0, …

---
+++ Dashboard/Modules/MATH-ANL/submit.php
@@ -0,0 +1,12 @@
+<?php
+
+echo $_POST['textarea'];
+
+?>
+
+<!-- MathJax -->
+<script type="text/x-mathjax-config">
+                  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});</script>
+<script type="text/javascript" async src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_CHTML"></script>
+
+<body style="padding:50px; width:40%; text-align:justify; margin-left:335px"></body>
---
+++ Dashboard/Modules/WYSIWYG_buttns.php
@@ -0,0 +1,78 @@
+<!-- <div id="wysiwyg_h2" style="padding-bottom:1px; width:100%; background:#eee; color:grey; border-left:1px solid lightgrey; border-right:1px solid lightgrey">
+
+  
+  <button type="button" class="btn btn-WTB" onclick="bold()">
+    <i class="fa fa-reply" aria-hidden="true"></i>
+  </button>
+
+  <button type="button" class="btn btn-WTB" onclick="bold()" 
+          style="border-right:1px solid #BDC3C7">
+    <i class="fa fa-share" aria-hidden="true"></i>
+  </button>
+  
+  <input  type="button"style="font-family:'Avenir','light'" value="Formats">
+  
+  <button type="button" class="btn btn-WTB" onclick="bold()" 
+          style="border-left:1px solid #BDC3C7;">
+    <i class="fa fa-font" aria-hidden="true"></i>
+  </button>
+
+  <button type="button" class="btn btn-WTB" onclick="bold()">
+    <i class="fa fa-bold" aria-hidden="true"></i>
+  </button>
+  
+  <button type="button" class="btn btn-WTB" onclick="italic()">
+    <i class="fa fa-italic" aria-hidden="true"></i>
+  </button>
+
+  <button type="button" class="btn btn-WTB" onclick="underline()">
+    <i class="fa fa-underline" aria-hidden="true"></i>
+  </button>
+
+  <button type="button" class="btn btn-WTB" onclick="bold()">
+    <i class="fa fa-n" aria-hidden="true">&Omega;</i>
+  </button>
+  
+  <button type="button" class="btn btn-WTB" onclick="bold()" 
+          type="button" style="border-left:1px solid #BDC3C7">
+    <i class="fa fa-align-left" aria-hidden="true"></i>
+  </button>
+  
+  <button type="button" class="btn btn-WTB" onclick="bold()">
+    <i class="fa fa-align-center" aria-hidden="true"></i>
+  </button>
+
+  <button type="button" class="btn btn-WTB" onclick="bold()">
+    <i class="fa fa-align-right" aria-hidden="true"></i>
+  </button>
+
+  <button class="btn btn-WTB" onclick="bold()">
+    <i class="fa fa-align-justify" aria-hidden="true"></i>
+  </button>
+
+  <button type="button" class="btn btn-WTB" onclick="bold()" style="border-left:1px solid #BDC3C7">
+    <i class="fa fa-list-ul" aria-hidden="true"></i>
+  </button>
+
+  <button type="button" class="btn btn-WTB" onclick="bold()" style="border-right:1px solid #BDC3C7">
+    <i class="fa fa-list-ol" aria-hidden="true"></i>
+  </button> 
+
+  <button type="button" class="btn btn-WTB" onclick="bold()">
+    <span class="glyphicon glyphicon-indent-right" aria-hidden="true"></span>
+  </button>
+
+  <button type="button" class="btn btn-WTB" onclick="bold()" style="border-right:1px solid #BDC3C7">
+    <span class="glyphicon glyphicon-indent-left" aria-hidden="true"></span>
+  </button>
+
+  <button type="button" class="btn btn-WTB" onclick="link()">
+    <i class="fa fa-link" aria-hidden="true"></i>
+  </button>
+  <button type="button" class="btn btn-WTB" onclick="link()">
+    <i class="fa fa-chain-broken" aria-hidden="true"></i>
+  </button>
+  <button type="button" class="btn btn-WTB" onclick="link()" style="margin-left:100px">
+    <span class="caret"></span>
+  </button>
+</div> -->
